71415cac60e55685b8179841b160f5b4ab7b232b
Simon Kaine <simonkaine@gmail.com>
2021-08-18 14:29:11 -0700

    Added title change ability + boolean dropdown refactored in details page + All tags should be working.

--- src/characterDetails.js
+++ src/characterDetails.js
@@ -2,35 +2,47 @@ import { Component } from 'react';
 import { getCharacter, getBreeds } from './fetch-utils';
 
 class CharacterDetails extends Component {
-    state = { id: 0, name: '', bad: Boolean, species: '' }
+    state = { id: 0, name: '', bad: false, species: '' }
+
     componentDidMount = async () => {
         const characterId = this.props.match.params.id;
         const characterData = await getCharacter(characterId);
         const speciesData = await getBreeds();
         this.setState({ ...characterData, speciesData });
+        console.log(this.state)
     }
+
     render() { 
         return ( 
             <>
             <h1>{this.state.name}</h1>
-            {/* <span>Is this person bad?</span> { this.state.bad ? "Yes" : "No" } */}
-                <form>
-                    <div className="details-data">
 
-                        <label>Is this person bad?</label>
-                        <select onChange={(e) => {
-                                this.setState({ bad: e.target.value });
-                            }} value={this.state.bad ? "true" : "false"}>
-                            <option value="false">No</option>
-                            <option value="true">Yes</option>
+                <form>
+
+                    <div className="details-data">
+                        <label>Character: </label>
+                        <input onChange={(event) => {
+                            this.setState({ name: event.target.value});
+                        }} type="text" value={this.state.name}>
+                        </input>
+                    </div>
+
+                    <div className="details-data">
+                        <label>Is this person bad? </label>
+                        <select 
+                                value={this.state.bad}
+                                onChange={ async (e) => {
+                                await this.setState({ bad: e.target.value });
+                            }}>
+                                <option value={true}>yes</option>
+                                <option value={false}>no</option>  
                         </select>
-                        
                     </div>
 
                     <div className="details-data">
                         <label>Species: </label>
-                            <select onChange={(e) => {
-                                this.setState({ species: e.target.value }); 
+                            <select onChange= { async (e) => {
+                                await this.setState({ species: e.target.value }); 
                                 }} value={this.state.species}>
                                 <option value="human">Human</option>
                                 <option value="plutonian">Plutonian</option>
--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -17,3 +17,15 @@ export const getBreeds = async (id) => {
     const data = await resp.json();
     return data;
 };
+
+export const updateCharacter = async (charObject) => {
+    const resp = await fetch(`${URL}/characters/${charObject.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'appllication/json',
+        },
+        body: JSON.stringify(charObject),
+    });
+    const data = await resp.json();
+    return data;
+};
